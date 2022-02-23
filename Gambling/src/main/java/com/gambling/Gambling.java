da7363693482e53410c83363bc916c68cc98dbd9
deven7818 <deven7818@gmail.com>
2022-02-23 12:07:38 +0530

    Gambling Simulation Problem Update uc1

--- Gambling/src/main/java/com/gambling/Gambling.java
+++ Gambling/src/main/java/com/gambling/Gambling.java
@@ -3,6 +3,9 @@ package com.gambling;
 import java.util.Scanner;
 
 public class Gambling {
+	
+	static final int STAKE_PAY = 100;
+	static final int PER_BET_GAME = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Enter the stake ammount");
